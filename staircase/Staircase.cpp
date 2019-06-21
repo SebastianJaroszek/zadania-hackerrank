d34f116983736d88ba1439fda4f26ce8be51d4f8
emjot <geecyrpg@gmail.com>
2019-06-21 22:37:09 +0200

    Metoda staircase została zaimplementowana.

--- staircase/Staircase.cpp
+++ staircase/Staircase.cpp
@@ -5,7 +5,11 @@ using namespace std;
 // Complete the staircase function below.
 void staircase(int n) {
 
-
+    for (int i = n; i > 0; i--) {
+        std::string staircase(n, '#');
+        string result = staircase.replace(0, i - 1, std::string(i - 1, ' '));
+        cout << result << endl;
+    }
 }
 
 int main()
