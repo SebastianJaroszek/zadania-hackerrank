6a9016695cbc6d7583ab44c6d674a67a8d82b64a
Sebastian Jaroszek <s.jaroszek@wspa.edu.pl>
2019-06-27 18:09:00 +0200

    Rozwiązanie do zadania Find Digits - implementacja metody findDigits.

--- find-digits/FindDigits.cpp
+++ find-digits/FindDigits.cpp
@@ -2,10 +2,33 @@
 
 using namespace std;
 
+/*
+korzystając z wiedzy o tym, że:
+5 to 12345 % 10
+4 to 12345 / 10 % 10
+3 to 12345 / 100 % 10
+2 to 12345 / 1000 % 10
+1 to 12345 / 10000 % 10
+*/
+
 // Complete the findDigits function below.
 int findDigits(int n) {
+    int numberLength = to_string(n).length();
 
+    int divisor = 1;
+    int result = 0;
+    for (int i = 0; i < numberLength; i++) {
+        int figure = (n / divisor) % 10;
+        if (figure == 0) {
+            divisor = divisor * 10;
+            continue;
+        } else if (n % figure == 0) {
+            result = result + 1;
+        }
+        divisor = divisor * 10;
+    }
 
+    return result;
 }
 
 int main()
